8faa6733635bced43e52208c149a867d0831b324
jizefeng <205402866@qq.com>
2025-06-01 15:40:48 +0800

    商品管理代码生成

---
+++ dkd-manage/src/main/java/com/dkd/manger/controller/SkuController.java
@@ -0,0 +1,104 @@
+package com.dkd.manger.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.dkd.common.annotation.Log;
+import com.dkd.common.core.controller.BaseController;
+import com.dkd.common.core.domain.AjaxResult;
+import com.dkd.common.enums.BusinessType;
+import com.dkd.manger.domain.Sku;
+import com.dkd.manger.service.ISkuService;
+import com.dkd.common.utils.poi.ExcelUtil;
+import com.dkd.common.core.page.TableDataInfo;
+
+/**
+ * 商品管理Controller
+ * 
+ * @author jzf
+ * @date 2025-06-01
+ */
+@RestController
+@RequestMapping("/manger/sku")
+public class SkuController extends BaseController
+{
+    @Autowired
+    private ISkuService skuService;
+
+    /**
+     * 查询商品管理列表
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(Sku sku)
+    {
+        startPage();
+        List<Sku> list = skuService.selectSkuList(sku);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出商品管理列表
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:export')")
+    @Log(title = "商品管理", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, Sku sku)
+    {
+        List<Sku> list = skuService.selectSkuList(sku);
+        ExcelUtil<Sku> util = new ExcelUtil<Sku>(Sku.class);
+        util.exportExcel(response, list, "商品管理数据");
+    }
+
+    /**
+     * 获取商品管理详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:query')")
+    @GetMapping(value = "/{skuId}")
+    public AjaxResult getInfo(@PathVariable("skuId") Long skuId)
+    {
+        return success(skuService.selectSkuBySkuId(skuId));
+    }
+
+    /**
+     * 新增商品管理
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:add')")
+    @Log(title = "商品管理", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody Sku sku)
+    {
+        return toAjax(skuService.insertSku(sku));
+    }
+
+    /**
+     * 修改商品管理
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:edit')")
+    @Log(title = "商品管理", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody Sku sku)
+    {
+        return toAjax(skuService.updateSku(sku));
+    }
+
+    /**
+     * 删除商品管理
+     */
+    @PreAuthorize("@ss.hasPermi('manger:sku:remove')")
+    @Log(title = "商品管理", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{skuIds}")
+    public AjaxResult remove(@PathVariable Long[] skuIds)
+    {
+        return toAjax(skuService.deleteSkuBySkuIds(skuIds));
+    }
+}
